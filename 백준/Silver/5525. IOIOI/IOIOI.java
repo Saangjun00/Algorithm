de0d9b4ce92efb1356c78d6df998046d1de9f0ab
Saangjun00 <149948808+Saangjun00@users.noreply.github.com>
2024-11-21 15:21:06 +0900

    [Silver I] Title: IOIOI, Time: 256 ms, Memory: 20124 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,24 +8,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());        // IOI 패턴 길이
+        int N = Integer.parseInt(br.readLine());        // OI 패턴 길이
         int M = Integer.parseInt(br.readLine());        // 문자열 길이
         String str = br.readLine();
 
-        String P = "I";     // 패턴
-        for (int i = 0; i < N; i++) {
-            P += "OI";
-        }
-
         int count = 0;
+        int result = 0;
 
-        for (int i = 0; i <= M - P.length(); i++) {
-            if (str.substring(i, i + P.length()).equals(P)) {
+        for (int i = 1; i < M - 1; i++) {
+            if (str.charAt(i - 1) == 'I' && str.charAt(i) == 'O' && str.charAt(i + 1) == 'I') {
                 count++;
+
+                if (count == N) {
+                    result++;
+                    count--;
+                }
+                i++;
+            }
+
+            else {
+                count = 0;
             }
         }
 
-        sb.append(count);
+        sb.append(result);
         System.out.println(sb);
     }
 }
